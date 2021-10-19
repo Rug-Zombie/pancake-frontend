4b49e77b54a775e43b03527ffe481cdcb482464e
CryptoWingnut <ninjawingnutcrypto@gmail.com>
2021-10-19 19:43:00 -0300

    feat: Code cleanup + rebase

--- src/views/Home/components/VictimPool/VictimPoolInfo.tsx
+++ src/views/Home/components/VictimPool/VictimPoolInfo.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { Card, Button, Heading, Dropdown } from '@rug-zombie-libs/uikit';
+import React, { useEffect } from 'react';
+import { Heading } from '@rug-zombie-libs/uikit';
 import { useTranslation } from 'contexts/Localization';
-import Select, { OptionProps } from 'components/Select/Select';
 import { useZTokenSwapper, useERC20 } from 'hooks/useContract';
 import { useWeb3React } from '@web3-react/core';
 import { multicallv2 } from 'utils/multicall';
@@ -12,41 +10,6 @@ import { getZTokenSwapperAddress } from 'utils/addressHelpers';
 import BigNumber from 'bignumber.js'
 import { BIG_ZERO } from 'utils/bigNumber';
 import { registerToken } from 'utils/wallet'
-
-const StyledVictimPoolsCard = styled(Card)`
-  background-image: url('/images/zmbe-bg.png');
-  background-size: 300px 300px;
-  background-position-x: 100px;
-  background-repeat: no-repeat;
-  background-position: top right;
-  min-height: 376px;
-  box-shadow: rgb(204 246 108) 0px 0px 20px;
-`
-
-const Block = styled.div`
-  margin-bottom: 16px;
-`
-
-const CardImage = styled.img`
-  margin-bottom: 16px;
-`
-
-const Label = styled.div`
-  color: ${({ theme }) => theme.colors.textSubtle};
-  font-size: 14px;
-`
-
-const Actions = styled.div`
-  margin-top: 24px;
-`
-
-const Row = styled.div`
-  align-items: center;
-  display: flex;
-  font-size: 14px;
-  justify-content: space-between;
-  margin-bottom: 8px;
-`
 
 export interface VictimPoolData {
     id: string, 
@@ -92,7 +55,6 @@ const VictimPoolsInfo: React.FC<VictimPoolsInfoProps> = ({ id }) => {
     const swapper = useZTokenSwapper();
     const address = getZTokenSwapperAddress();
     const { account } = useWeb3React();
-    const { t } = useTranslation();
     const pool = pools.find(a => a.id === id);
     const rug = useERC20(pool.rug);
     const ztoken = useERC20(pool.ztoken);
--- src/views/Home/components/VictimPool/VictimPoolsCard.tsx
+++ src/views/Home/components/VictimPool/VictimPoolsCard.tsx
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { Card, CardBody, Heading, Dropdown } from '@rug-zombie-libs/uikit';
+import { Card, CardBody, Heading } from '@rug-zombie-libs/uikit';
 import { useTranslation } from 'contexts/Localization';
 import Select, { OptionProps } from 'components/Select/Select';
-import { useZTokenSwapper, useERC20 } from 'hooks/useContract';
-import { useWeb3React } from '@web3-react/core';
-import { multicallv2 } from 'utils/multicall';
-import erc20Abi from 'config/abi/erc20.json';
-import { getZTokenSwapperAddress } from 'utils/addressHelpers';
-import VictimPoolsInfo, { VictimPoolData } from './VictimPoolInfo';
+import VictimPoolsInfo from './VictimPoolInfo';
 
 const StyledVictimPoolsCard = styled(Card)`
   background-image: url('/images/zmbe-bg.png');
@@ -19,28 +14,7 @@ const StyledVictimPoolsCard = styled(Card)`
   min-height: 376px;
   box-shadow: rgb(204 246 108) 0px 0px 20px;
 `
-
-const Block = styled.div`
-  margin-bottom: 16px;
-`
-
-const CardImage = styled.img`
-  margin-bottom: 16px;
-`
-
-const Label = styled.div`
-  color: ${({ theme }) => theme.colors.textSubtle};
-  font-size: 14px;
-`
-
-const Actions = styled.div`
-  margin-top: 24px;
-`
-
 const VictimPoolsCard = () => {
-    const swapper = useZTokenSwapper();
-    const address = getZTokenSwapperAddress();
-    const { account } = useWeb3React();
     const { t } = useTranslation();    
 
     const options = [
